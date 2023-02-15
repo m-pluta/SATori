ab6696bfa27b37e2ee6eaa09f8f27156eadbd1ab
Michal Pluta <michalpl2003@gmail.com>
2023-02-15 21:22:32 +0000

    Review comments in code and switch to the faster check_truth_assignment function, Use timeit

--- vsdc48.py
+++ vsdc48.py
@@ -25,17 +25,17 @@ def load_dimacs(filepath):
     return clauses
 
 
-# Return satisfying truth assignment for given clause set or return False if it isnt satisfiable
+
+# Return satisfying truth assignment for given clause set or return False if it isn't satisfiable
 def simple_sat_solve(clause_set):
     # Find all variables in the clause set (a variable and it's complement are the same)
     variables = np.unique(np.array([np.abs(literal) for clause in clause_set for literal in clause]))
-    num_variables = len(variables)
 
-    # Generate all possible truth assignment permutations of length=num_variables
+    # Generate all possible truth assignment permutations of length=len(variables)
     # e.g. for length=3, [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
-    truth_assignments = list(product([1, -1], repeat=num_variables))
+    truth_assignments = list(product([1, -1], repeat=len(variables)))
   
-    # Loop through all possible truth assignment and return it if it satisfies the clause set
+    # Loop through all possible truth assignments and return it if it satisfies the clause set
     for truth_assignment in truth_assignments:
         # Combine each variable with its corresponding truth value
         assignment = [var*truth for var,truth in zip(variables,truth_assignment)]
@@ -44,28 +44,20 @@ def simple_sat_solve(clause_set):
         if check_truth_assignment(clause_set, assignment):
             return assignment
     
-    # None of the truth_assignments satisfy the clause set
+    # None of the truth assignments satisfy the clause set
     return False
 
-
-# Return true if the truth assignment satisfies the given clause set
 def check_truth_assignment(clause_set, assignment):
     setA = set(assignment)
     # Loop through each clause
     for clause in clause_set:
-        setB = set(clause)
         # If the clause and truth assignment do not have a literal in common then clause is not satisfied
-        if (not setA.intersection(setB)):
+        if (setA.isdisjoint(clause)):
             return False
     # Return true if all clauses were satisfied
     return True
 
-def check_truth_assignment_fast(clause_set, assignment):
-    setA = set(assignment)
-    for clause in clause_set:
-        if (setA.isdisjoint(clause)):
-            return False
-    return True
+
 
 def branching_sat_solve(clause_set, partial_assignment=[]):
     # Find all variables in the clause set (a variable and it's complement are the same)
@@ -74,7 +66,6 @@ def branching_sat_solve(clause_set, partial_assignment=[]):
     result = branching_sat_solve_wrapped(clause_set, partial_assignment, variables)
 
     return result if result else False
-
 
 def branching_sat_solve_wrapped(clause_set, partial_assignment, variables):
     if check_truth_assignment(clause_set, partial_assignment):
@@ -91,6 +82,8 @@ def branching_sat_solve_wrapped(clause_set, partial_assignment, variables):
             return potential_solution
 
     return []
+
+
 
 def unit_propagate(clause_set, unit_literals=None):
     if unit_literals == None:
@@ -141,18 +134,23 @@ def unit_propagate(clause_set, unit_literals=None):
     else:
         return clause_set
 
-# Testing
+
+
 
 # clauses = load_dimacs('instances/unsat.txt')
-clauses = load_dimacs('instances/sat.txt')
-# clauses = load_dimacs('instances/W_2,3_ n=8.txt')
+# clauses = load_dimacs('instances/sat.txt')
+clauses = load_dimacs('instances/W_2,3_ n=8.txt')
 # clauses = load_dimacs('instances/PHP-5-4.txt')
 # clauses = load_dimacs('instances/LNP-6.txt')
 # clauses = load_dimacs('instances/8queens.txt')
 
+
 # print(unit_propagate([[1,2,3],[2,3],[1],[3],[5],[7,8,9],[-1,-3,-5,11],[-3,13], [-1,-5, 6],[2,4,5]]))
 # print(unit_propagate(clauses))
-print(clauses)
 
-
-print(branching_sat_solve(clauses))
+# import timeit
+# assignment = [1, 2, -3, -4, 5, 6, -7, -8, -9, -10, 11, 12, -13, -14, 15, 16]
+# print('Slow')
+# print(np.mean(np.array(timeit.repeat('check_truth_assignment(clauses, assignment)', globals=globals(), number=100, repeat=10000))))
+# print('Fast')
+# print(np.mean(np.array(timeit.repeat('check_truth_assignment_fast(clauses, assignment)', globals=globals(), number=100, repeat=10000))))
