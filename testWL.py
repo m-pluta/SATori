b8abacce92c9c2dead875cd5783666c3ff66bc99
Michal Pluta <michalpl2003@gmail.com>
2023-03-09 21:49:30 +0000

    Improve sat tester

--- testWL.py
+++ testWL.py
@@ -15,20 +15,10 @@ def load_dimacs(filepath):
             clauses.append([int(literal) for literal in line.split() if literal != '0'])
     return clauses
 
-print("uf20")
-for fileCount in range(1, 1001):
-    fileName = f'sat_instances/uf20/uf20-0{fileCount}.cnf'
-    print(fileCount)
-    sat_instance = load_dimacs(fileName)
-    result = dpll_sat_solve(sat_instance)
-    if not result:
-        print(fileName)
-        sys.exit()
-
-print("flat50")
-for fileCount in range(1, 1000):
-    if fileCount != 73:
-        fileName = f'sat_instances/flat50/flat50-{fileCount}.cnf'
+def uf20():
+    print("uf20")
+    for fileCount in range(1, 1001):
+        fileName = f'sat_instances/uf20/uf20-0{fileCount}.cnf'
         print(fileCount)
         sat_instance = load_dimacs(fileName)
         result = dpll_sat_solve(sat_instance)
@@ -36,30 +26,47 @@ for fileCount in range(1, 1000):
             print(fileName)
             sys.exit()
 
-print("CBS")
-for clauseCount in [403]:
-    for backboneSize in [10, 30, 50, 70, 90]:
-        mainFileName = f'CBS_k3_n100_m{clauseCount}_b{backboneSize}'
-        for (fileCount) in range(0, 1000):
+def flat50():
+    print("flat50")
+    for fileCount in range(1, 1000):
+        if fileCount != 73:
+            fileName = f'sat_instances/flat50/flat50-{fileCount}.cnf'
             print(fileCount)
-            fileName = f'sat_instances/{mainFileName}/{mainFileName}_{fileCount}.cnf'
             sat_instance = load_dimacs(fileName)
             result = dpll_sat_solve(sat_instance)
             if not result:
                 print(fileName)
                 sys.exit()
 
-print("sw100")
-for p in range(0,2):
-    mainFileName = f'sat_instances/sw100-8-lp{p}-c5/SW100-8-{p}/sw100'
-    for (fileCount) in range(1, 101):
-        if fileCount != 16:
-            print(fileCount)
-            fileName = f'{mainFileName}-{fileCount}.cnf'
-            sat_instance = load_dimacs(fileName)
-            result = dpll_sat_solve(sat_instance)
-            if not result:
-                print(fileName)
-                sys.exit()
-            
+def CBS():
+    print("CBS")
+    for clauseCount in [403]:
+        for backboneSize in [10, 30, 50, 70, 90]:
+            mainFileName = f'CBS_k3_n100_m{clauseCount}_b{backboneSize}'
+            for (fileCount) in range(0, 1000):
+                print(fileCount)
+                fileName = f'sat_instances/{mainFileName}/{mainFileName}_{fileCount}.cnf'
+                sat_instance = load_dimacs(fileName)
+                result = dpll_sat_solve(sat_instance)
+                if not result:
+                    print(fileName)
+                    sys.exit()
 
+def sw100():
+    print("sw100")
+    for p in range(0,2):
+        mainFileName = f'sat_instances/sw100-8-lp{p}-c5/SW100-8-{p}/sw100'
+        for (fileCount) in range(1, 101):
+            if fileCount != 16:
+                print(fileCount)
+                fileName = f'{mainFileName}-{fileCount}.cnf'
+                sat_instance = load_dimacs(fileName)
+                result = dpll_sat_solve(sat_instance)
+                if not result:
+                    print(fileName)
+                    sys.exit()
+
+uf20()
+flat50()
+CBS()
+# sw100()
